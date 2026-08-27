f8ba96de1e19d74eb97f81955bbc0df74654a9d8
angelinanegrette25-dotcom <angelinanegrette25@gmail.com>
2026-08-27 14:29:51 +0000

    feat:  implemnte nuva clase, Gestor de Proyectos

--- ejersisio.py/enclase.py
+++ ejersisio.py/enclase.py
@@ -11,3 +11,33 @@ class Colaborador:
             print("Aviso: el colaborador ya existe.")
         else:
             self.colaboradores.append(colaborador)
+
+
+    def tiene_colaborador(self, username: str) -> bool:
+        for colaborador in self.colaboradores:
+            if colaborador.username == username:
+                return True
+        return False
+    
+    def listar_colaboradores(self) -> list[Colaborador]:
+        return self.colaboradores
+
+
+    def __str__(self) -> str:
+            return (
+                f"Proyecto: {self.nombre} "
+                f"[{self.lenguaje}] — {len(self.colaboradores)} colaboradores"
+            )
+
+
+class GestorProyectos:
+
+    def __init__(self) -> None:
+        self.proyectos: list[Proyecto] = []
+
+    def registrar_proyecto(self, proyecto: Proyecto) -> None:
+
+        if self.buscar_proyecto(proyecto.nombre) is not None:
+            print("Aviso: el proyecto ya existe.")
+        else:
+            self.proyectos.append(proyecto)
